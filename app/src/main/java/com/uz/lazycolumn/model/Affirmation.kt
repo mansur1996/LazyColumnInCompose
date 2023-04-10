01f4eefcfe06394a2b39768473a0341bc79c3a3a
@mirzayevmn96 <mirzayevmn96@gmail.com>
2023-04-10 12:22:25 +0500

    first commit

---
+++ app/src/main/java/com/uz/lazycolumn/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.uz.lazycolumn.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId : Int,
+    @DrawableRes val imageResourceId : Int,
+)
